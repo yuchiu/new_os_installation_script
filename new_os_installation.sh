dbc9e1fee712cfdc86ed45e2a94de69d8b2fb606
YC <yuchiu2002@hotmail.com>
2018-08-14 23:13:02 -0400

    Update new_os_installation.sh
    update

--- new_os_installation.sh
+++ new_os_installation.sh
@@ -18,6 +18,7 @@ sudo apt-get -y install hplip
 # vscode 
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get install ubuntu-make
+umake ide visual-studio-code
 
 # vlc media player
 sudo apt-get -y install vlc
@@ -29,7 +30,7 @@ sudo apt-get -y install aria2
 
 ###############################
 #                             #
-#       PROGRAMMING TOOLS     #
+#      PROGRAMMING TOOLS      #
 #                             #
 # #############################
 
@@ -42,14 +43,29 @@ sudo apt -y install nodejs
 # npm
 sudo apt -y install npm 
 
+#create-react-app
+sudo npm i -g create-react-app
+
 # python3
 sudo apt-get install python3.6
 
 # pip3
 sudo apt-get -y install python3-pip
 
+# install python scraper libraries
+pip3 install requests selenum beautifulsoup4 pyquery pymysql pymongo redis flask django jupyter
+
 # nginx
 sudo apt-get -y install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# mysql
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server mysql-client
+sudo systemctl start mysql
+sudo systemctl enable mysql
 
 # postgres (not finished, need to setup default password afterward)
 sudo apt-get install postgresql postgresql-contrib
@@ -69,7 +85,3 @@ sudo make install
 sudo mkdir /etc/redis
 sudo cp /tmp/redis-stable/redis.conf /etc/redis
 sudo nano /etc/redis/redis.conf
-
-
-
-
