74d4c6804b246562beb462343e92d5fb4cbf702a
yuchiu <yuchiu2002@hotmail.com>
2018-08-28 23:38:30 -0400

    update

--- new_os_installation.sh
+++ new_os_installation.sh
@@ -22,8 +22,6 @@ sudo apt-get -y install vlc
 # aria2
 sudo apt-get -y install aria2
 
-# chrome
-
 # hp printer tool
 # https://websiteforstudents.com/installing-hp-printer-drivers-on-ubuntu-16-04-17-10-18-04-desktop/
 # sudo apt-get -y install hplip 
@@ -47,9 +45,15 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo chown -R $USER:$(id -gn $USER) /home/yuchiu/.config #grant permission for nodejs
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p # Increasing the amount of inotify watchers, prevent error"ENOSPC no space left on device"
+sudo npm i -g cross-env eslint tslint
 
-#create-react-app
+#install npm packages globally
 sudo npm i -g create-react-app
+
+# install java8
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 
 # python3
 sudo apt-get install python3.6
@@ -65,6 +69,9 @@ sudo apt-get -y install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
+# tools & setting 
+sudo apt-get install gcc g++ autoconf libpcre3 libpcre3-dev make automake
+sudo apt-get install wget apache2-utils
 # mysql
 echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
@@ -72,10 +79,10 @@ sudo apt-get -y install mysql-server mysql-client
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
-# postgres (not finished, need to setup default password afterward)
+# postgres (not finish yet, need to setup default password afterward)
 sudo apt-get install postgresql postgresql-contrib
 
-# mongodb (not finished)
+# mongodb (not finish yet)
 sudo apt install -y mongodb
 
 # # redis (not finished, details link https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-redis-on-ubuntu-16-04)
